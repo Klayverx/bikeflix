9cab1b4c2f167c59a8b4ffc11f64961de9d2d2b9
Klayver Ximenes <klayverx@gmail.com>
2020-07-28 22:09:00 -0300

    commit pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter
+} from "react-router-dom";
+
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  // rotas dos navegadores
+  <BrowserRouter>
+
+  {/* As possíveis entradas */}
+    <Switch>
+      
+      {/* rotas possuem prioridade por ordem listada no código quando coloca-se (exact) */}
+      <Route path="/cadastro/video" component={CadastroVideo} />
+
+      <Route path="/" component={Home} exact />
+
+      {/* a última rota sem o "path" é sempre carregada caso nao caia em nenhuma outra */}
+      <Route component={Pagina404} />
+
+    </Switch>
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
