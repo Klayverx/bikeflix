5666c6452d59c9a5c7f327df6bb900d70a903b81
Klayver Ximenes <klayverx@gmail.com>
2021-05-20 11:54:38 -0300

    refac

--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -1,28 +1,27 @@
-import URL_BACKEND from '../config';
+import URL_BACKEND from '../config'
 
-const URL_CATEGORIES = `${URL_BACKEND}/categorias`;
+const URL_CATEGORIES = `${URL_BACKEND}/categorias`
 
 function getAll() {
-  return fetch(`${URL_CATEGORIES}`).then(async (response) => {
-    if (response.ok) {
-      const resposta = await response.json();
-      return resposta;
-    }
+	return fetch(`${URL_CATEGORIES}`).then(async response => {
+		if (response.ok) {
+			const resposta = await response.json()
+			return resposta
+		}
 
-    throw new Error('Error Unknow');
-  });
+		throw new Error('Error Unknow')
+	})
 }
 
-function getAllWithVideos() {
-  return fetch(`${URL_CATEGORIES}?_embed=videos`)
-    .then(async (response) => {
-      if (response.ok) {
-        const resposta = await response.json();
-        return resposta;
-      }
+async function getAllWithVideos() {
+	return await fetch(`${URL_CATEGORIES}?_embed=videos`).then(async response => {
+		if (response.ok) {
+			const resposta = await response.json()
+			return resposta
+		}
 
-      throw new Error('Error Unknow');
-    });
+		throw new Error('Error Unknow')
+	})
 }
 
-export default { getAll, getAllWithVideos };
+export default { getAll, getAllWithVideos }
