d7e306946f5629632092485b4405ac445e625de5
Klayver Ximenes <klayverx@gmail.com>
2021-05-20 11:50:09 -0300

    refac

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,53 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import PageDefault from '../../components/PageDefault';
-import Carousel from '../../components/Carousel';
-import BannerMain from '../../components/BannerMain';
-import categoriasRepository from '../../repositories/categorias';
+import React, { useState, useEffect } from 'react'
+import PageDefault from '../../components/PageDefault'
+import Carousel from '../../components/Carousel'
+import BannerMain from '../../components/BannerMain'
+import categoriasRepository from '../../repositories/categorias'
 
 function Home() {
-  const [dadosIniciais, setdadosIniciais] = useState([]);
+	const [dadosIniciais, setdadosIniciais] = useState([])
 
-  useEffect(() => {
-    categoriasRepository
-      .getAllWithVideos()
-      .then((item) => {
-        setdadosIniciais(item);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
+	useEffect(() => {
+		categoriasRepository
+			.getAllWithVideos()
+			.then(item => {
+				setdadosIniciais(item)
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	}, [])
 
-  return (
-    <PageDefault>
-      {dadosIniciais.length === 0 && (<div>Loading...</div>)}
+	return (
+		<PageDefault>
+			{dadosIniciais.length === 0 && <div>Loading...</div>}
 
-      {dadosIniciais.map((categoria, indice) => {
-        if (indice === 0) {
-          return (
-            <div key={categoria.id}>
-              <BannerMain
-                videoTitle={dadosIniciais[0].videos[0].titulo}
-                url={dadosIniciais[0].videos[0].url}
-                videoDescription={dadosIniciais[0].videos[0].description}
-              />
-              <Carousel
-                ignoreFirstVideo
-                category={dadosIniciais[0]}
-              />
-            </div>
-          );
-        }
+			{dadosIniciais.map((categoria, indice) => {
+				if (indice === 0) {
+					return (
+						<div key={categoria.id}>
+							<BannerMain
+								videoTitle={dadosIniciais[0].videos[0].titulo}
+								url={dadosIniciais[0].videos[0].url}
+								videoDescription={dadosIniciais[0].videos[0].description}
+							/>
+							<Carousel ignoreFirstVideo category={dadosIniciais[0]} />
+						</div>
+					)
+				}
 
-        return (
-          <Carousel
-            key={categoria.id}
-            category={categoria}
-          />
-        );
-      })}
-    </PageDefault>
-  );
+				return <Carousel key={categoria.id} category={categoria} />
+			})}
+		</PageDefault>
+	)
 }
 
-export default Home;
+export default Home
