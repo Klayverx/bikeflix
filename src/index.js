a842a8c0ccb89a567df0a29134a1f94711c1da90
Klayver Ximenes <klayverx@gmail.com>
2021-05-20 11:49:17 -0300

    refac

--- src/index.js
+++ src/index.js
@@ -1,31 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import CadastroVideo from './pages/cadastro/Video';
-import CadastroCategoria from './pages/cadastro/Categoria';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria'
 
-const Pagina404 = () => (<div>Página 404</div>);
+const Pagina404 = () => <div>Página 404</div>
 
 ReactDOM.render(
+	// rotas dos navegadores
+	<BrowserRouter>
+		<Switch>
+			<Route path="/" component={Home} exact />
+			<Route path="/cadastro/video" component={CadastroVideo} />
+			<Route path="/cadastro/categoria" component={CadastroCategoria} />
 
-  // rotas dos navegadores
-  <BrowserRouter>
+			{/* a última rota sem o "path" é sempre carregada caso nao caia em nenhuma outra */}
+			<Route component={Pagina404} />
+		</Switch>
+	</BrowserRouter>,
 
-    {/* As possíveis entradas */}
-    <Switch>
-      {/* rotas possuem prioridade por ordem listada no código quando coloca-se (exact) */}
-      <Route path="/" component={Home} exact />
-      <Route path="/cadastro/video" component={CadastroVideo} />
-      <Route path="/cadastro/categoria" component={CadastroCategoria} />
-
-      {/* a última rota sem o "path" é sempre carregada caso nao caia em nenhuma outra */}
-      <Route component={Pagina404} />
-
-    </Switch>
-  </BrowserRouter>,
-
-  document.getElementById('root'),
-);
+	document.getElementById('root')
+)
