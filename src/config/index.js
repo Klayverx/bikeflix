d427d4ad9237ea0141b4a9bc7b1611e785038f47
Klayver Ximenes <klayverx@gmail.com>
2021-05-20 11:54:14 -0300

    att URL_BACKEND

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,5 @@
 const URL_BACKEND = window.location.href.includes('localhost')
-  ? 'http://localhost:8080/categorias'
-  : 'http://localhost:8080';
+	? 'http://localhost:8080'
+	: 'http://localhost:8080/categorias'
 
-export default {
-  URL_BACKEND,
-};
+export default URL_BACKEND
