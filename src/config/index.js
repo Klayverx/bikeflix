7a48a10a93045c02e79821c76b94d835a4f6822b
Klayver Ximenes <klayverx@gmail.com>
2021-05-20 12:18:05 -0300

    att URL_BACKEND

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,3 @@
-const URL_BACKEND = window.location.href.includes('localhost')
-	? 'http://localhost:8080'
-	: 'https://devsoutinhoflix.herokuapp.com'
+const URL_BACKEND = 'https://devsoutinhoflix.herokuapp.com'
 
 export default URL_BACKEND
