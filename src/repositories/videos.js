0665f41825824e6c5a5db137a5bf408bc0640084
Klayver Ximenes <klayverx@gmail.com>
2021-05-20 11:52:39 -0300

    refac

--- src/repositories/videos.js
+++ src/repositories/videos.js
@@ -1,22 +1,22 @@
-import URL_BACKEND from '../config';
+import URL_BACKEND from '../config'
 
-const URL_CATEGORIES = `${URL_BACKEND}/categorias`;
+const URL_CATEGORIES = `${URL_BACKEND}/categorias`
 
 function create(object) {
-  return fetch(`${URL_CATEGORIES}`, {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify(object),
-  }).then(async (response) => {
-    if (response.ok) {
-      const res = await response.json();
-      return res;
-    }
+	return fetch(`${URL_CATEGORIES}`, {
+		method: 'POST',
+		headers: {
+			'Content-type': 'application/json',
+		},
+		body: JSON.stringify(object),
+	}).then(async response => {
+		if (response.ok) {
+			const res = await response.json()
+			return res
+		}
 
-    throw new Error('Error Unknow on register');
-  });
+		throw new Error('Error Unknow on register')
+	})
 }
 
-export default { create };
+export default { create }
